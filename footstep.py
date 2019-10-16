a8d43e4e9c08641c494cc71a953dca41e7874420
Yifan <38448751+Hsing1996@users.noreply.github.com>
2019-10-16 00:10:41 -0400

    Update footstep.py

--- footstep.py
+++ footstep.py
@@ -3,13 +3,19 @@ import cvxopt
 import matplotlib.pyplot as plt
 
 # T is the length of time interval
-# t[k] is the discrete time 
+# t is the discrete time of each step
 
-T = 200
-A = np.matrix('1 T T^2/2; 0 1 T; 0 0 1')
-B = np.matrix('T^3/6 T^2/2 T')
+def nextstep(T, t, x, x1, x2, x3 ,y, y1, y2, y3 ):
+    k = T/t; # number of steps
+    X = np.array([x, x1, x2]).T
+    # position info on x
+    Y = np.array([x, x1, x2]).T
+    # position info on y
 
-x_hat = 
-# vector of position, speed and acceleration on x
-y_hat = 
-# vector of position, speed and acceleration on x
+    A = np.matrix('1 T T^2/2; 0 1 T; 0 0 1')
+    B = np.matrix('T^3/6 T^2/2 T')
+
+    for i in range(k):
+        X = X
+        Y = 
+    
