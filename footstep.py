3532d8ae6e644383c06233e15fe310d90e85ae48
Yifan <38448751+Hsing1996@users.noreply.github.com>
2019-10-16 12:09:04 -0400

    Update footstep.py

--- footstep.py
+++ footstep.py
@@ -8,15 +8,21 @@ import norm
 
 def nextstep(T, t, x, x1, x2, x3 ,y, y1, y2, y3 ):
     k = T/t; # number of steps
-    X = np.array([x, x1, x2]).T
-    # position info on x
-    Y = np.array([x, x1, x2]).T
-    # position info on y
-
+   
     A = np.matrix('1 T T^2/2; 0 1 T; 0 0 1')
     B = np.matrix('T^3/6 T^2/2 T')
 
-    for i in range(k):
-        X = X
-        Y = 
-    
+    X = np.empty(shape=[3,k])
+    Y = np.empty(shape=[3,k])
+    #put all vectors in a matrix
+
+    X[0] = np.matrix([x, x1, x2]).T
+    # position info on x
+    Y[0] = np.matrix([x, x1, x2]).T
+    # position info on y
+
+    for i in range (k):
+        X[:,i+1] = A * X[:,i] + B * x3
+        Y[:,i+1] = A * Y[:,i] + B * y3
+        print('Future foot step in x direction: \n ' + X, end=' ')
+        print('Future foot step in y direction: \n ' + Y)
